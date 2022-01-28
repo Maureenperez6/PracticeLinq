cdfdfd209451a8f115dfc2e0b1731f75b347be9b
Maureen Perez <maureen8694@gmail.com>
2022-01-28 14:39:15 -0500

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGames = new List<string> { "Super Smash Bros.", "Mario Party", "Mario Kart" };
+            
+            //videoGames.ForEach(name => Console.WriteLine(name));
+
+            var VG = videoGames.OrderBy(name => name.Length);
+            foreach (var name in videoGames)
+            {
+                Console.WriteLine(name);
+            }
+
+           //LINQ way
+           //videoGames.OrderBy(name => name.Length).ToList().ForEach(x => Console.WriteLine(x));
+
+        }
+    }
+}
